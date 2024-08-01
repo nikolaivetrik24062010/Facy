c2806ac6ee403bd541b5f1124e715ac32550e46a
Vetrik <vetrik21@gmail.com>
2024-08-01 14:34:17 -0700

    Added logic in MainActivity

--- app/src/main/java/com/example/facy/MainActivity.java
+++ app/src/main/java/com/example/facy/MainActivity.java
@@ -1,20 +1,75 @@
 package com.example.facy;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.FirebaseApp;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.face.FaceDetection;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    TextView textView;
+    ImageView imageView;
+
+    private final static int REQUEST_IMAGE_CAPTURE = 124;
+    InputImage firebaseVisionImage;
+    FaceDetection visionFaceDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.cameraBtn);
+        textView = findViewById(R.id.text1);
+        imageView = findViewById(R.id.image);
+
+        FirebaseApp.initializeApp(this);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFile();
+            }
+        });
+        Toast.makeText(this, "App is started", Toast.LENGTH_SHORT).show();
+    }
+
+    private void OpenFile() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+        } else {
+            Toast.makeText(this, "Failed!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bundle bundle = data.getExtras();
+        Bitmap bitmap = (Bitmap) bundle.get("data");
+        FaceDetectionProcess(bitmap);
+        Toast.makeText(this, "Success", Toast.LENGTH_SHORT).show();
+    }
+
+    private void FaceDetectionProcess(Bitmap bitmap) {
 
     }
 }
